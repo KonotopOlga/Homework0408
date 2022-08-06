28e2ebbc7075726d7bb4126f7a5b0beabedfdcf3
KonotopOlga <ol.konotop@yandex.ru>
2022-08-06 15:00:24 +0300

    zadacha4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
-﻿int a = 22;
+﻿/* 
+int a = 22;
 int b = 3;
 int c = 9;
+int max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+Console.WriteLine ( max ); //
+
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+Console.WriteLine ( max );
+*/
+int a = 44;
+int b = 5;
+int c = 78;
 int max = a;
 if ( b > max ) max = b;
 if ( c > max ) max = c;
